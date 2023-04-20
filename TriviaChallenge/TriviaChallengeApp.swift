bc12a9df44371e2faa2872576bc0bef74358fb8b
Isaad Khan <IKhan@lcs.on.ca>
2023-04-20 10:01:54 -0400

    Initial Commit

---
+++ TriviaChallenge/TriviaChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaChallengeApp.swift
+//  TriviaChallenge
+//
+//  Created by Isaad Khan on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct TriviaChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
